edd6705561cb8a7f799fbe61de674038c5ba1781
GabrielGBernardes <gabrielgber2@gmail.com>
2020-07-04 16:02:31 -0300

    versao 1.1

--- src/controllers/WhatsAppController.ts
+++ src/controllers/WhatsAppController.ts
@@ -3,7 +3,7 @@ import connection from '../connection'
 import routes from '../routes'
 
 export default class WhatsAppController {
-    async create(req: Request, res: Response) {
+    async createTextMessage(req: Request, res: Response) {
         const { telefone, mensagem } = req.body
 
         const { Client, TextContent } = require('@zenvia/sdk');
@@ -14,13 +14,33 @@ export default class WhatsAppController {
 
         whatsapp.sendMessage('polished-vessel', telefone, content)
         .then((response: any) => {
-            console.log(response)
+            //console.log(response)
+            res.status(200).send(response)
         })
         .catch((error: any) => {
-            console.log(error)
+            res.status(400).send(error)
+            //console.log(error)
         })
     }
+
+    async createFileMessage(req: Request, res: Response) {
+        const { telefone, url, extencao, descricao } = req.body
+
+        const { Client, FileContent } = require('@zenvia/sdk');
+        
+        const client = new Client('kQWxWxvmEMlG0o_LsGDoJ0G3D7q693TCf1o4');
+        const whatsapp = client.getChannel('whatsapp');
+        const content = new FileContent(url, extencao, descricao);
+
+        whatsapp.sendMessage('polished-vessel', telefone, content)
+        .then((response: any) => {
+            //console.log(response)
+            res.status(200).send(response)
+        })
+        .catch((error: any) => {
+            res.status(400).send(error)
+            //console.log(error)
+        })
+    }
+
 }
-
-
-
--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,12 @@
 
 import express from 'express'
 
-import ClientController from './controllers/ClientController'
 import WhatsAppController from './controllers/WhatsAppController'
 
-//import SMSController from './controllers/SMSController'
-//import SOSNumbersController from './controllers/SOSNumbersController'
-
 const routes = express.Router()
-const usersController = new ClientController()
 const wppController = new WhatsAppController()
-//const smsController = new SMSController()
-//const sosController = new SOSNumbersController()
 
-routes.post('/wpp', wppController.create)
-
-routes.get('/cliet', usersController.index)
-routes.get('/cliet/:telefone', usersController.show)
-routes.post('/cliet', usersController.create)
+routes.post('/wpp/text', wppController.createTextMessage)
+routes.post('/wpp/file', wppController.createFileMessage)
 
 export default routes
