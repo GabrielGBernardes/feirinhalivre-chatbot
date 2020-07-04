1b607d4a9e4b5b1757ca27f325b717b1f01fb992
GabrielGBernardes <gabrielgber2@gmail.com>
2020-07-04 00:32:50 -0300

    end edit exemples

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,17 @@
 
 import express from 'express'
 
-import UsersController from './controllers/UsersController'
-import SMSController from './controllers/SMSController'
-import SOSNumbersController from './controllers/SOSNumbersController'
+import ClientController from './controllers/ClientController'
+//import SMSController from './controllers/SMSController'
+//import SOSNumbersController from './controllers/SOSNumbersController'
 
 const routes = express.Router()
-const usersController = new UsersController()
-const smsController = new SMSController()
-const sosController = new SOSNumbersController()
+const usersController = new ClientController()
+//const smsController = new SMSController()
+//const sosController = new SOSNumbersController()
 
-routes.get('/users', usersController.index)
-routes.get('/user/:telefone', usersController.show)
-routes.post('/user', usersController.create)
-routes.post('/token', smsController.create)
-
-routes.get('/sos/:estado', sosController.show)
+routes.get('/cliet', usersController.index)
+routes.get('/cliet/:telefone', usersController.show)
+routes.post('/cliet', usersController.create)
 
 export default routes
