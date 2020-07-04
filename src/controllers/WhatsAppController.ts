58022e9c717878c97ce2cd6b5b72d77bba51291a
GabrielGBernardes <gabrielgber2@gmail.com>
2020-07-04 11:23:34 -0300

    Add route wpp for WhatsAppController

--- src/controllers/WhatsAppController.ts
+++ src/controllers/WhatsAppController.ts
@@ -3,14 +3,14 @@ import connection from '../connection'
 import routes from '../routes'
 
 export default class WhatsAppController {
-    async index(req: Request, res: Response) {
-        const { params: { telefone } } = req
+    async create(req: Request, res: Response) {
+        const { params: { telefone, mensagem } } = req
 
         const { Client, TextContent } = require('@zenvia/sdk');
         
         const client = new Client('kQWxWxvmEMlG0o_LsGDoJ0G3D7q693TCf1o4');
         const whatsapp = client.getChannel('whatsapp');
-        const content = new TextContent('some text message here');
+        const content = new TextContent(mensagem);
 
         whatsapp.sendMessage('polished-vessel', telefone, content)
         .then((response: any) => {
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,18 @@
 import express from 'express'
 
 import ClientController from './controllers/ClientController'
+import WhatsAppController from './controllers/WhatsAppController'
+
 //import SMSController from './controllers/SMSController'
 //import SOSNumbersController from './controllers/SOSNumbersController'
 
 const routes = express.Router()
 const usersController = new ClientController()
+const wppController = new WhatsAppController()
 //const smsController = new SMSController()
 //const sosController = new SOSNumbersController()
+
+routes.post('/wpp', wppController.create)
 
 routes.get('/cliet', usersController.index)
 routes.get('/cliet/:telefone', usersController.show)
